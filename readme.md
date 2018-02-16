72df654e6910cd7dfef8f169a963f5936d8cc63b
htmelvis <ed@htmelvis.com>
2018-02-16 15:29:25 -0500

    additional babel assets and webpack.config additions

--- readme.md
+++ readme.md
@@ -8,6 +8,7 @@
 - ES6
 - React/ReactDOM
 - Yarn Pkg Manager
+- eslint (prettier, react, airbnb)
 
 ```nvm use``` 
 Above command sets your current node version to the version in .nvmrc file. This should be v8.8.1
@@ -19,5 +20,11 @@ Command requires yarn which can be installed by using npm (npm install yarn). Ta
 ```yarn start```
 Kicks off the development server and environment for creating the front end components 
 
+### Front End Tech
+- React
+- Stylus
+- PostCSS
+- Styled Components
+- CSSGrid / Flexbox
 
 
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,8 +1,51 @@
 import webpack from 'webpack';
+import path from 'path';
 
 const config = {
-
+  context: __dirname,
+  entry: [
+    './js/entry/app'
+  ],
+  devtool: 'cheap-eval-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  devServer: {
+    hot: true,
+    publicPath: '/dist/',
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: false
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+  module: {
+    rules: [
+      { enforce: 'pre', test: /\.jsx?$/, loader: "eslint-loader", exclude: /node_modules/ },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ],
+  }
 };
 
-export default config;
+if (process.env.NODE_ENV === 'development') {
+  config.entry.unshift('webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000');
+}
 
+export default config;
